71e7a0a0126cdab699390e50d1b8a50a79bcd8ab
Soumik29 <61573712+Soumik29@users.noreply.github.com>
2023-04-24 23:33:20 -0400

    Science Form
    Science form uploaded

---
+++ EasyQuiz/Science.Designer.vb
@@ -0,0 +1,231 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Science
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Question1 = New System.Windows.Forms.Label()
+        Me.Question2 = New System.Windows.Forms.Label()
+        Me.Question3 = New System.Windows.Forms.Label()
+        Me.Question4 = New System.Windows.Forms.Label()
+        Me.Question5 = New System.Windows.Forms.Label()
+        Me.Question6 = New System.Windows.Forms.Label()
+        Me.Ans1 = New System.Windows.Forms.TextBox()
+        Me.Ans2 = New System.Windows.Forms.TextBox()
+        Me.Ans3 = New System.Windows.Forms.TextBox()
+        Me.Ans4 = New System.Windows.Forms.TextBox()
+        Me.txtScore = New System.Windows.Forms.TextBox()
+        Me.Ans5 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Question1
+        '
+        Me.Question1.AutoSize = True
+        Me.Question1.Location = New System.Drawing.Point(79, 111)
+        Me.Question1.Name = "Question1"
+        Me.Question1.Size = New System.Drawing.Size(199, 20)
+        Me.Question1.TabIndex = 0
+        Me.Question1.Text = "What is the nearest Galexy"
+        '
+        'Question2
+        '
+        Me.Question2.AutoSize = True
+        Me.Question2.Location = New System.Drawing.Point(79, 176)
+        Me.Question2.Name = "Question2"
+        Me.Question2.Size = New System.Drawing.Size(216, 20)
+        Me.Question2.TabIndex = 1
+        Me.Question2.Text = "What is th formula for Water?"
+        '
+        'Question3
+        '
+        Me.Question3.AutoSize = True
+        Me.Question3.Location = New System.Drawing.Point(79, 234)
+        Me.Question3.Name = "Question3"
+        Me.Question3.Size = New System.Drawing.Size(278, 20)
+        Me.Question3.TabIndex = 2
+        Me.Question3.Text = "What is the closest planet to the Sun?"
+        '
+        'Question4
+        '
+        Me.Question4.AutoSize = True
+        Me.Question4.Location = New System.Drawing.Point(79, 298)
+        Me.Question4.Name = "Question4"
+        Me.Question4.Size = New System.Drawing.Size(341, 20)
+        Me.Question4.TabIndex = 3
+        Me.Question4.Text = "What is the hottest planet in our Solar System?"
+        '
+        'Question5
+        '
+        Me.Question5.AutoSize = True
+        Me.Question5.Location = New System.Drawing.Point(79, 354)
+        Me.Question5.Name = "Question5"
+        Me.Question5.Size = New System.Drawing.Size(261, 20)
+        Me.Question5.TabIndex = 4
+        Me.Question5.Text = "What is the powerhouse of the cell?"
+        '
+        'Question6
+        '
+        Me.Question6.AutoSize = True
+        Me.Question6.Location = New System.Drawing.Point(79, 422)
+        Me.Question6.Name = "Question6"
+        Me.Question6.Size = New System.Drawing.Size(51, 20)
+        Me.Question6.TabIndex = 5
+        Me.Question6.Text = "Score"
+        '
+        'Ans1
+        '
+        Me.Ans1.Location = New System.Drawing.Point(491, 108)
+        Me.Ans1.Name = "Ans1"
+        Me.Ans1.Size = New System.Drawing.Size(100, 26)
+        Me.Ans1.TabIndex = 6
+        '
+        'Ans2
+        '
+        Me.Ans2.Location = New System.Drawing.Point(491, 176)
+        Me.Ans2.Name = "Ans2"
+        Me.Ans2.Size = New System.Drawing.Size(100, 26)
+        Me.Ans2.TabIndex = 7
+        '
+        'Ans3
+        '
+        Me.Ans3.Location = New System.Drawing.Point(491, 234)
+        Me.Ans3.Name = "Ans3"
+        Me.Ans3.Size = New System.Drawing.Size(100, 26)
+        Me.Ans3.TabIndex = 8
+        '
+        'Ans4
+        '
+        Me.Ans4.Location = New System.Drawing.Point(491, 292)
+        Me.Ans4.Name = "Ans4"
+        Me.Ans4.Size = New System.Drawing.Size(100, 26)
+        Me.Ans4.TabIndex = 9
+        '
+        'txtScore
+        '
+        Me.txtScore.Location = New System.Drawing.Point(491, 419)
+        Me.txtScore.Name = "txtScore"
+        Me.txtScore.Size = New System.Drawing.Size(100, 26)
+        Me.txtScore.TabIndex = 10
+        '
+        'Ans5
+        '
+        Me.Ans5.Location = New System.Drawing.Point(491, 354)
+        Me.Ans5.Name = "Ans5"
+        Me.Ans5.Size = New System.Drawing.Size(100, 26)
+        Me.Ans5.TabIndex = 11
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(647, 99)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(152, 35)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Show Answer"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(647, 170)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(152, 32)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Clear Answer"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(647, 234)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(152, 36)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Check Answer"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(647, 295)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(152, 36)
+        Me.Button4.TabIndex = 15
+        Me.Button4.Text = "Exit"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(647, 357)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(152, 35)
+        Me.Button5.TabIndex = 16
+        Me.Button5.Text = "Main Menu"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Science
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(887, 502)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Ans5)
+        Me.Controls.Add(Me.txtScore)
+        Me.Controls.Add(Me.Ans4)
+        Me.Controls.Add(Me.Ans3)
+        Me.Controls.Add(Me.Ans2)
+        Me.Controls.Add(Me.Ans1)
+        Me.Controls.Add(Me.Question6)
+        Me.Controls.Add(Me.Question5)
+        Me.Controls.Add(Me.Question4)
+        Me.Controls.Add(Me.Question3)
+        Me.Controls.Add(Me.Question2)
+        Me.Controls.Add(Me.Question1)
+        Me.Name = "Science"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Question1 As Label
+    Friend WithEvents Question2 As Label
+    Friend WithEvents Question3 As Label
+    Friend WithEvents Question4 As Label
+    Friend WithEvents Question5 As Label
+    Friend WithEvents Question6 As Label
+    Friend WithEvents Ans1 As TextBox
+    Friend WithEvents Ans2 As TextBox
+    Friend WithEvents Ans3 As TextBox
+    Friend WithEvents Ans4 As TextBox
+    Friend WithEvents txtScore As TextBox
+    Friend WithEvents Ans5 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+End Class
---
+++ EasyQuiz/Science.vb
@@ -0,0 +1,66 @@
+﻿Public Class Science
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Question6.Click
+
+    End Sub
+
+    Private Sub Science_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Ans1.Clear()
+        Ans2.Clear()
+        Ans3.Clear()
+        Ans4.Clear()
+        Ans5.Clear()
+        txtScore.Clear()
+    End Sub
+
+    Dim Ans100, Ans200, Ans300, Ans400, Ans500 As String
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim mainForm As New MainForm
+        Me.Hide()
+        mainForm.Show()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+
+        Ans100 = "Andromeda"
+        Ans200 = "H20"
+        Ans300 = "Mercury"
+        Ans400 = "Venus"
+        Ans500 = "Mitochondria"
+        Dim score As Integer
+        score = 0
+        If Ans1.Text = Ans100 Then
+            score += 1
+        End If
+        If Ans2.Text = Ans200 Then
+            score += 1
+        End If
+        If Ans3.Text = Ans300 Then
+            score += 1
+        End If
+        If Ans4.Text = Ans400 Then
+            score += 1
+        End If
+        If Ans5.Text = Ans500 Then
+            score += 1
+        End If
+        txtScore.Text = CStr(score) + "/5"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Ans1.Text = Ans100
+        Ans2.Text = Ans200
+        Ans3.Text = Ans300
+        Ans4.Text = Ans400
+        Ans5.Text = Ans500
+    End Sub
+End Class
